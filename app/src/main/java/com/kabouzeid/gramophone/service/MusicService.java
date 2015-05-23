2a7dad6ad00d21f2a0e3f9d7a42e8be5fb429640
Karim Abou Zeid <karim23697@gmail.com>
2015-05-23 12:44:14 +0200

    Small bug fix.

--- app/src/main/java/com/kabouzeid/gramophone/service/MusicService.java
+++ app/src/main/java/com/kabouzeid/gramophone/service/MusicService.java
@@ -796,7 +796,7 @@ public class MusicService extends Service implements MediaPlayer.OnPreparedListe
         //to let other apps know whats playing. i.E. last.fm (scrobbling) or musixmatch
         final Intent intent = new Intent(what);
         final int position = getPosition();
-        if(position >= 0) {
+        if (position >= 0 && !playingQueue.isEmpty()) {
             final Song currentSong = playingQueue.get(position);
             intent.putExtra("id", currentSong.id);
             intent.putExtra("artist", currentSong.artistName);
