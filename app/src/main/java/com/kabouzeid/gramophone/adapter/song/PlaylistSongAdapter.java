f903b862d917b58fcc3b69169234626b84176ff2
Karim Abou Zeid <karim23697@gmail.com>
2015-07-13 02:35:38 +0200

    Fixed playlist song adapter wrong song menu

--- app/src/main/java/com/kabouzeid/gramophone/adapter/song/PlaylistSongAdapter.java
+++ app/src/main/java/com/kabouzeid/gramophone/adapter/song/PlaylistSongAdapter.java
@@ -3,7 +3,6 @@ package com.kabouzeid.gramophone.adapter.song;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.util.Pair;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.view.View;
@@ -13,8 +12,6 @@ import com.kabouzeid.gramophone.dialogs.RemoveFromPlaylistDialog;
 import com.kabouzeid.gramophone.interfaces.CabHolder;
 import com.kabouzeid.gramophone.model.PlaylistSong;
 import com.kabouzeid.gramophone.model.Song;
-import com.kabouzeid.gramophone.ui.activities.base.AbsFabActivity;
-import com.kabouzeid.gramophone.util.NavigationUtil;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -31,6 +28,11 @@ public class PlaylistSongAdapter extends SongAdapter {
     public PlaylistSongAdapter(@NonNull AppCompatActivity activity, @NonNull ArrayList<PlaylistSong> dataSet, @LayoutRes int itemLayoutRes, @Nullable CabHolder cabHolder) {
         super(activity, (ArrayList<Song>) (List) dataSet, itemLayoutRes, cabHolder);
         overrideMultiSelectMenuRes(R.menu.menu_playlists_songs_selection);
+    }
+
+    @Override
+    protected SongAdapter.ViewHolder createViewHolder(View view) {
+        return new ViewHolder(view);
     }
 
     @Override
@@ -58,14 +60,6 @@ public class PlaylistSongAdapter extends SongAdapter {
             switch (item.getItemId()) {
                 case R.id.action_remove_from_playlist:
                     RemoveFromPlaylistDialog.create((PlaylistSong) getSong()).show(activity.getSupportFragmentManager(), "REMOVE_FROM_PLAYLIST");
-                    return true;
-                case R.id.action_go_to_album:
-                    Pair[] albumPairs = new Pair[]{
-                            Pair.create(image, activity.getString(R.string.transition_album_art))
-                    };
-                    if (activity instanceof AbsFabActivity)
-                        albumPairs = ((AbsFabActivity) activity).getSharedViewsWithFab(albumPairs);
-                    NavigationUtil.goToAlbum(activity, ((PlaylistSong) getSong()).albumId, albumPairs);
                     return true;
             }
             return super.onSongMenuItemClick(item);
