d3cf342ee754be0d4bd6bca1797dbc2b0073902b
Karim Abou Zeid <karim23697@gmail.com>
2015-08-26 18:05:12 +0200

    Added option to support the development by donating.

--- app/src/main/java/com/kabouzeid/gramophone/App.java
+++ app/src/main/java/com/kabouzeid/gramophone/App.java
@@ -19,6 +19,8 @@ import io.fabric.sdk.android.Fabric;
 public class App extends Application {
     public static final String TAG = App.class.getSimpleName();
 
+    public static final String GOOGLE_PLAY_LICENSE_KEY = "MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAjMeADN5Ffnt/ml5SYxNPCn8kGcOYGpHEfNSCts99vVxqmCn6C01E94c17j7rUK2aeHur5uxphZylzopPlQ8P8l1fqty0GPUNRSo18FCJzfGH8HZAwZYOcnRFPaXdaq3InyFJhBiODh2oeAcVK/idH6QraQ4r9HIlzigAg6lgwzxl2wJKDh7X/GMdDntCyzDh8xDQ0wIawFgvgojHwqh2Ci8Gnq6EYRwPA9yHiIIksT8Q30QyM5ewl5QcnWepsls7enNqeHarhpmSibRUDgCsxHoOpny7SyuvZvUI3wuLckDR0ds9hrt614scHHqDOBp/qWCZiAgOPVAEQcURbV09qQIDAQAB";
+
     private RefWatcher refWatcher;
 
     public static RefWatcher getRefWatcher(Context context) {
