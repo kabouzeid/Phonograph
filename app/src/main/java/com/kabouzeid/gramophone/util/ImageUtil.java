9b2aaea2c9b097d17b5391efc289a5191145d3f5
Karim Abou Zeid <karim23697@gmail.com>
2015-09-22 16:44:37 +0200

    Fixed kabouzeid/phonograph-issue-tracker#4, when setting an album cover in the tag editor, the image will be cropped instead of stretched now.

--- app/src/main/java/com/kabouzeid/gramophone/util/ImageUtil.java
+++ app/src/main/java/com/kabouzeid/gramophone/util/ImageUtil.java
@@ -1,27 +1,9 @@
 package com.kabouzeid.gramophone.util;
 
-import android.graphics.Bitmap;
-import android.graphics.Matrix;
-import android.support.annotation.NonNull;
-
 /**
  * @author Karim Abou Zeid (kabouzeid)
  */
 public class ImageUtil {
-
-    public static Bitmap getResizedBitmap(@NonNull Bitmap bm, int newHeight, int newWidth, boolean recycleOld) {
-        int width = bm.getWidth();
-        int height = bm.getHeight();
-        float scaleWidth = ((float) newWidth) / width;
-        float scaleHeight = ((float) newHeight) / height;
-        Matrix matrix = new Matrix();
-        matrix.postScale(scaleWidth, scaleHeight);
-        Bitmap resizedBitmap = Bitmap.createBitmap(bm, 0, 0, width, height, matrix, false);
-        if (recycleOld && resizedBitmap != bm) {
-            bm.recycle();
-        }
-        return resizedBitmap;
-    }
 
     public static int calculateInSampleSize(int width, int height, int reqWidth) {
         // setting reqWidth matching to desired 1:1 ratio and screen-size
