93cb5f872114d9a5ff5d6d3df4876957adf5caf6
Karim Abou Zeid <karim23697@gmail.com>
2015-12-23 17:36:20 +0100

    Fixed deprecation

--- app/src/main/java/com/kabouzeid/gramophone/dialogs/ClearSmartPlaylistDialog.java
+++ app/src/main/java/com/kabouzeid/gramophone/dialogs/ClearSmartPlaylistDialog.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.text.Html;
 
+import com.afollestad.materialdialogs.DialogAction;
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.model.smartplaylist.AbsSmartPlaylist;
@@ -37,15 +38,15 @@ public class ClearSmartPlaylistDialog extends LeakDetectDialogFragment {
                 .content(content)
                 .positiveText(R.string.clear_action)
                 .negativeText(android.R.string.cancel)
-                .callback(new MaterialDialog.ButtonCallback() {
+                .onPositive(new MaterialDialog.SingleButtonCallback() {
                     @Override
-                    public void onPositive(MaterialDialog dialog) {
-                        super.onPositive(dialog);
+                    public void onClick(@NonNull MaterialDialog dialog, @NonNull DialogAction which) {
                         if (getActivity() == null) {
                             return;
                         }
                         playlist.clear(getActivity());
                     }
-                }).build();
+                })
+                .build();
     }
 }
