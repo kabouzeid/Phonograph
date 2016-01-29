6255ac3e29083fb690c49673c4bd2deb483ef4f9
Karim Abou Zeid <karim23697@gmail.com>
2016-01-29 11:57:37 +0100

    Added option to save a playlist as m3u file.

---
+++ app/src/main/java/com/kabouzeid/gramophone/helper/M3UConstants.java
@@ -0,0 +1,8 @@
+package com.kabouzeid.gramophone.helper;
+
+public interface M3UConstants {
+    String EXTENSION = "m3u";
+    String HEADER = "#EXTM3U";
+    String ENTRY = "#EXTINF:";
+    String DURATION_SEPARATOR = ",";
+}
---
+++ app/src/main/java/com/kabouzeid/gramophone/helper/M3UWriter.java
@@ -0,0 +1,42 @@
+package com.kabouzeid.gramophone.helper;
+
+import android.content.Context;
+
+import com.kabouzeid.gramophone.loader.PlaylistSongLoader;
+import com.kabouzeid.gramophone.model.Playlist;
+import com.kabouzeid.gramophone.model.PlaylistSong;
+import com.kabouzeid.gramophone.model.Song;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class M3UWriter implements M3UConstants {
+    public static final String TAG = M3UWriter.class.getSimpleName();
+
+    public static File write(Context context, File dir, Playlist playlist) throws IOException {
+        if (!dir.exists()) //noinspection ResultOfMethodCallIgnored
+            dir.mkdirs();
+        File file = new File(dir, playlist.name.concat("." + EXTENSION));
+
+        ArrayList<PlaylistSong> songs = PlaylistSongLoader.getPlaylistSongList(context, playlist.id);
+
+        if (songs.size() > 0) {
+            BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+            bw.write(HEADER);
+            for (Song song : songs) {
+                bw.newLine();
+                bw.write(ENTRY + song.duration + DURATION_SEPARATOR + song.artistName + " - " + song.title);
+                bw.newLine();
+                bw.write(song.data);
+            }
+
+            bw.close();
+        }
+
+        return file;
+    }
+}
