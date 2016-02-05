9b2835694dbe8c2c9dee3836ec6197a45675b4e4
Karim Abou Zeid <karim23697@gmail.com>
2016-02-06 00:41:45 +0100

    Updated libs and gradle.

--- app/src/main/java/com/kabouzeid/gramophone/lastfm/rest/LastFMRestClient.java
+++ app/src/main/java/com/kabouzeid/gramophone/lastfm/rest/LastFMRestClient.java
@@ -15,8 +15,8 @@ import okhttp3.Interceptor;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
 import okhttp3.Response;
-import retrofit2.GsonConverterFactory;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 
 /**
