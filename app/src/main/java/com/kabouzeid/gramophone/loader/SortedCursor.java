1acac3ec5e82673a1e7633401d6fec60c11a06c6
Karim Abou Zeid <karim23697@gmail.com>
2016-02-18 19:00:58 +0100

    Updated gradle plugin

--- app/src/main/java/com/kabouzeid/gramophone/loader/SortedCursor.java
+++ app/src/main/java/com/kabouzeid/gramophone/loader/SortedCursor.java
@@ -32,7 +32,6 @@ import java.util.List;
  */
 public class SortedCursor extends AbstractCursor {
     // cursor to wrap
-    @Nullable
     private final Cursor mCursor;
     // the map of external indices to internal indices
     private ArrayList<Integer> mOrderedPositions;
@@ -48,7 +47,7 @@ public class SortedCursor extends AbstractCursor {
      * @param order      the list of unique ids in sorted order to display
      * @param columnName the column name of the id to look up in the internal cursor
      */
-    public SortedCursor(@Nullable final Cursor cursor, final long[] order, final String columnName,
+    public SortedCursor(final Cursor cursor, final long[] order, final String columnName,
                         final List<?> extraData) {
         if (cursor == null) {
             throw new IllegalArgumentException("Non-null cursor is needed");
