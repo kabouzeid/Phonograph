ca4222ac8df83f95d49f4c806fc14e33319716b2
Karim Abou Zeid <karim23697@gmail.com>
2016-03-26 14:36:49 +0100

    Enabled crashlytics for debug builds.

--- app/src/main/java/com/kabouzeid/gramophone/App.java
+++ app/src/main/java/com/kabouzeid/gramophone/App.java
@@ -3,7 +3,6 @@ package com.kabouzeid.gramophone;
 import android.app.Application;
 
 import com.crashlytics.android.Crashlytics;
-import com.crashlytics.android.core.CrashlyticsCore;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -19,9 +18,8 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        // Set up Crashlytics, disabled for debug builds
         Crashlytics crashlyticsKit = new Crashlytics.Builder()
-                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build())
+//                .core(new CrashlyticsCore.Builder().disabled(BuildConfig.DEBUG).build()) // Set up Crashlytics, disabled for debug builds
                 .build();
         Fabric.with(this, crashlyticsKit);
     }
