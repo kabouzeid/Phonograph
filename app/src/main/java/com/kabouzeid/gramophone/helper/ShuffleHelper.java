dfb011a93d54082c6a98796284083e97fb73b264
Karim Abou Zeid <karim23697@gmail.com>
2016-05-30 00:22:29 +0200

    Fixed an ArrayIndexOutOfBoundsException.

--- app/src/main/java/com/kabouzeid/gramophone/helper/ShuffleHelper.java
+++ app/src/main/java/com/kabouzeid/gramophone/helper/ShuffleHelper.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class ShuffleHelper {
 
     public static void makeShuffleList(@NonNull List<Song> listToShuffle, final int current) {
+        if (listToShuffle.isEmpty()) return;
         if (current >= 0) {
             Song song = listToShuffle.remove(current);
             Collections.shuffle(listToShuffle);
