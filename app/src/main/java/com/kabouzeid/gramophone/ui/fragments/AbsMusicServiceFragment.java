d07056c8acda16bc1709bbf7842c168fb46b7004
ezio84 <brabus84@gmail.com>
2017-04-14 13:08:23 +0200

    Fix toolbar buttons (favicon, close, 3dots) being invisible on white covers
    add statusbar and toolbar opacity (black, 0.1f) so the buttons are always
    visible.

--- app/src/main/java/com/kabouzeid/gramophone/ui/fragments/AbsMusicServiceFragment.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/fragments/AbsMusicServiceFragment.java
@@ -1,12 +1,17 @@
 package com.kabouzeid.gramophone.ui.fragments;
 
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.View;
 
+import com.kabouzeid.appthemehelper.ATH;
+import com.kabouzeid.appthemehelper.util.ColorUtil;
 import com.kabouzeid.gramophone.interfaces.MusicServiceEventListener;
+import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.ui.activities.base.AbsMusicServiceActivity;
+import com.kabouzeid.gramophone.ui.activities.MainActivity;
 
 /**
  * @author Karim Abou Zeid (kabouzeid)
@@ -14,13 +19,18 @@ import com.kabouzeid.gramophone.ui.activities.base.AbsMusicServiceActivity;
 public class AbsMusicServiceFragment extends Fragment implements MusicServiceEventListener {
     private AbsMusicServiceActivity activity;
 
+    public MainActivity getMainActivity() {
+        return (MainActivity) getActivity();
+    }
+
     @Override
     public void onAttach(Context context) {
         super.onAttach(context);
         try {
             activity = (AbsMusicServiceActivity) context;
         } catch (ClassCastException e) {
-            throw new RuntimeException(context.getClass().getSimpleName() + " must be an instance of " + AbsMusicServiceActivity.class.getSimpleName());
+            throw new RuntimeException(context.getClass().getSimpleName() + " must be an instance of "
+                    + AbsMusicServiceActivity.class.getSimpleName());
         }
     }
 
@@ -80,5 +90,36 @@ public class AbsMusicServiceFragment extends Fragment implements MusicServiceEve
     @Override
     public void onMediaStoreChanged() {
 
+    }
+
+    /**
+     * This will set the color of the view with the id "status_bar" on KitKat and Lollipop.
+     * On Lollipop if no such view is found it will set the statusbar color using the native method.
+     *
+     * @param color the new statusbar color (will be shifted down on Lollipop and above)
+     */
+    public void setStatusbarColor(int color) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            final View statusBar = activity.getWindow().getDecorView().getRootView().findViewById(R.id.status_bar);
+            if (statusBar != null) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                    statusBar.setBackgroundColor(ColorUtil.darkenColor(color));
+                    setLightStatusbarAuto(color);
+                } else {
+                    statusBar.setBackgroundColor(color);
+                }
+            } else if (Build.VERSION.SDK_INT >= 21) {
+                activity.getWindow().setStatusBarColor(ColorUtil.darkenColor(color));
+                setLightStatusbarAuto(color);
+            }
+        }
+    }
+
+    public void setLightStatusbar(boolean enabled) {
+        ATH.setLightStatusbar(activity, enabled);
+    }
+
+    public void setLightStatusbarAuto(int bgColor) {
+        setLightStatusbar(ColorUtil.isColorLight(bgColor));
     }
 }
