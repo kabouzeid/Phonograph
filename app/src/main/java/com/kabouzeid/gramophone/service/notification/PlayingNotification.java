41b81ce14c328e5a8346afaa11f5b2a6d1e52ed1
Lincoln <lincolnbernard7@gmail.com>
2017-08-14 23:04:59 -0400

    Swipeable classic notification (#244)
    * Made notification swipeable when not playing not swipeable when playing. Removed "X" close button
    
    * Fixed music stop on tap and on app close
    
    * Refactor notification classes

--- app/src/main/java/com/kabouzeid/gramophone/service/notification/PlayingNotification.java
+++ app/src/main/java/com/kabouzeid/gramophone/service/notification/PlayingNotification.java
@@ -1,17 +1,55 @@
 package com.kabouzeid.gramophone.service.notification;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+
 import com.kabouzeid.gramophone.service.MusicService;
 
-/**
- * @author Karim Abou Zeid (kabouzeid)
- */
+import static android.content.Context.NOTIFICATION_SERVICE;
 
-public interface PlayingNotification {
-    int NOTIFICATION_ID = 1;
+public abstract class PlayingNotification {
 
-    void init(MusicService service);
+    private static final int NOTIFICATION_ID = 1;
+    private static final int NOTIFY_MODE_FOREGROUND = 1;
+    private static final int NOTIFY_MODE_BACKGROUND = 0;
 
-    void update();
+    private int notifyMode = NOTIFY_MODE_BACKGROUND;
 
-    void stop();
+    private NotificationManager notificationManager;
+    protected MusicService service;
+    boolean stopped;
+
+    public synchronized void init(MusicService service) {
+        this.service = service;
+        notificationManager = (NotificationManager) service.getSystemService(NOTIFICATION_SERVICE);
+    }
+
+    abstract public void update();
+
+    public synchronized void stop() {
+        stopped = true;
+        service.stopForeground(true);
+        notificationManager.cancel(NOTIFICATION_ID);
+    }
+
+    void updateNotifyModeAndPostNotification(Notification notification) {
+        int newNotifyMode;
+        if (service.isPlaying()) {
+            newNotifyMode = NOTIFY_MODE_FOREGROUND;
+        } else {
+            newNotifyMode = NOTIFY_MODE_BACKGROUND;
+        }
+
+        if (notifyMode != newNotifyMode && newNotifyMode == NOTIFY_MODE_BACKGROUND) {
+            service.stopForeground(false);
+        }
+
+        if (newNotifyMode == NOTIFY_MODE_FOREGROUND) {
+            service.startForeground(NOTIFICATION_ID, notification);
+        } else if (newNotifyMode == NOTIFY_MODE_BACKGROUND) {
+            notificationManager.notify(NOTIFICATION_ID, notification);
+        }
+
+        notifyMode = newNotifyMode;
+    }
 }
