7feb9e4d3f393318238ca508a5f0ede9717862f0
Eugene Cheung <ey.cheung@outlook.com>
2017-09-09 11:54:38 -0400

    Properly sized icons in head unit

--- app/src/main/java/com/kabouzeid/gramophone/service/MusicService.java
+++ app/src/main/java/com/kabouzeid/gramophone/service/MusicService.java
@@ -559,22 +559,22 @@ public class MusicService extends Service implements SharedPreferences.OnSharedP
     }
 
     private void setCustomAction(PlaybackStateCompat.Builder stateBuilder) {
-        int repeatIcon = R.drawable.ic_repeat_white_nocircle_24dp;  // REPEAT_MODE_NONE
+        int repeatIcon = R.drawable.ic_repeat_white_nocircle_48dp;  // REPEAT_MODE_NONE
         if (getRepeatMode() == REPEAT_MODE_THIS) {
-            repeatIcon = R.drawable.ic_repeat_one_white_circle_24dp;
+            repeatIcon = R.drawable.ic_repeat_one_white_circle_48dp;
         } else if (getRepeatMode() == REPEAT_MODE_ALL) {
-            repeatIcon = R.drawable.ic_repeat_white_circle_24dp;
+            repeatIcon = R.drawable.ic_repeat_white_circle_48dp;
         }
         stateBuilder.addCustomAction(new PlaybackStateCompat.CustomAction.Builder(
                 CYCLE_REPEAT, getString(R.string.action_cycle_repeat), repeatIcon)
                 .build());
 
-        final int shuffleIcon = getShuffleMode() == SHUFFLE_MODE_NONE ? R.drawable.ic_shuffle_white_nocircle_24dp : R.drawable.ic_shuffle_white_circle_24dp;
+        final int shuffleIcon = getShuffleMode() == SHUFFLE_MODE_NONE ? R.drawable.ic_shuffle_white_nocircle_48dp : R.drawable.ic_shuffle_white_circle_48dp;
         stateBuilder.addCustomAction(new PlaybackStateCompat.CustomAction.Builder(
                 TOGGLE_SHUFFLE, getString(R.string.action_toggle_shuffle), shuffleIcon)
                 .build());
 
-        final int favoriteIcon = MusicUtil.isFavorite(getApplicationContext(), getCurrentSong()) ? R.drawable.ic_favorite_white_circle_24dp : R.drawable.ic_favorite_border_white_nocircle_24dp;
+        final int favoriteIcon = MusicUtil.isFavorite(getApplicationContext(), getCurrentSong()) ? R.drawable.ic_favorite_white_circle_48dp : R.drawable.ic_favorite_border_white_nocircle_48dp;
         stateBuilder.addCustomAction(new PlaybackStateCompat.CustomAction.Builder(
                 TOGGLE_FAVORITE, getString(R.string.action_toggle_favorite), favoriteIcon)
                 .build());
