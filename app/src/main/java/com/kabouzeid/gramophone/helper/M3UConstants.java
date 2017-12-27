b74c4eced6b66b5c357c3e29fe90ee1b23374838
AdrienPoupa <adrien@poupa.fr>
2017-12-27 23:49:52 +0100

    Clean repo

--- app/src/main/java/com/kabouzeid/gramophone/helper/M3UConstants.java
+++
@@ -1,8 +0,0 @@
-package com.kabouzeid.gramophone.helper;
-
-public interface M3UConstants {
-    String EXTENSION = "m3u";
-    String HEADER = "#EXTM3U";
-    String ENTRY = "#EXTINF:";
-    String DURATION_SEPARATOR = ",";
-}
