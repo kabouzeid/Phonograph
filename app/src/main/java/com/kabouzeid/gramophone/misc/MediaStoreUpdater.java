3c6083e5f7135793f9717ad5c6d216da5d501230
doompadee <31240866+doompadee@users.noreply.github.com>
2018-01-02 17:51:42 +0100

    Only perform scan when not already running, cleanup

--- app/src/main/java/com/kabouzeid/gramophone/misc/MediaStoreUpdater.java
+++ app/src/main/java/com/kabouzeid/gramophone/misc/MediaStoreUpdater.java
@@ -4,7 +4,9 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
@@ -13,13 +15,17 @@ import com.kabouzeid.gramophone.R;
 
 public class MediaStoreUpdater {
     public static void confirmUpdate(Context context) {
-        new MaterialDialog.Builder(context)
-                .title(R.string.scan_whole_file_system)
-                .content(R.string.scan_warning)
-                .positiveText(R.string.scan_action)
-                .negativeText(android.R.string.cancel)
-                .onPositive((dialog, action) -> update(context))
-                .show();
+        if (isMediaScannerRunning(context)) {
+            Toast.makeText(context, R.string.scan_already_updating, Toast.LENGTH_SHORT).show();
+        } else {
+            new MaterialDialog.Builder(context)
+                    .title(R.string.scan_whole_file_system)
+                    .content(R.string.scan_warning)
+                    .positiveText(R.string.scan_action)
+                    .negativeText(android.R.string.cancel)
+                    .onPositive((dialog, action) -> update(context))
+                    .show();
+        }
     }
 
     public static void update(Context context) {
@@ -38,14 +44,22 @@ public class MediaStoreUpdater {
         filter.addDataScheme("file");
         filter.addAction(Intent.ACTION_MEDIA_SCANNER_STARTED);
         filter.addAction(Intent.ACTION_MEDIA_SCANNER_FINISHED);
-        ScanReceiver receiver = new ScanReceiver();
-        context.registerReceiver(receiver, filter);
+        context.registerReceiver(new ScanReceiver(), filter);
 
         Bundle args = new Bundle();
         args.putString("volume", "external");
         Intent scannerIntent = new Intent("android.media.IMediaScannerService")
-                .setClassName("com.android.providers.media","com.android.providers.media.MediaScannerService")
+                .setClassName("com.android.providers.media", "com.android.providers.media.MediaScannerService")
                 .putExtras(args);
         context.startService(scannerIntent);
+    }
+
+    private static boolean isMediaScannerRunning(Context context) {
+        try (Cursor query = context.getContentResolver().query(MediaStore.getMediaScannerUri(), new String[]{MediaStore.MEDIA_SCANNER_VOLUME}, null, null, null)) {
+            if (query != null && query.moveToFirst()) {
+                return query.getString(query.getColumnIndex(MediaStore.MEDIA_SCANNER_VOLUME)) != null;
+            }
+        }
+        return false;
     }
 }
