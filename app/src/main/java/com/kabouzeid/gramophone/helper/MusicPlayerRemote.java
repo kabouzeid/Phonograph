e58354bba8044ab98d4efc9cf33361c507af63fa
Andrew <andrew@vaccaro.io>
2018-06-18 21:27:17 -0400

    Fixes

--- app/src/main/java/com/kabouzeid/gramophone/helper/MusicPlayerRemote.java
+++ app/src/main/java/com/kabouzeid/gramophone/helper/MusicPlayerRemote.java
@@ -177,8 +177,9 @@ public class MusicPlayerRemote {
     public static void openQueue(final ArrayList<Song> queue, final int startPosition, final boolean startPlaying) {
         if (!tryToHandleOpenPlayingQueue(queue, startPosition, startPlaying) && musicService != null) {
             musicService.openQueue(queue, startPosition, startPlaying);
-            if (!PreferenceUtil.getInstance(musicService).rememberShuffle())
+            if (!PreferenceUtil.getInstance(musicService).rememberShuffle()){
                 setShuffleMode(MusicService.SHUFFLE_MODE_NONE);
+            }
         }
     }
 
