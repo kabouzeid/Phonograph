6464307c7dacb0509a49866dd4edf67c9e4c1450
Karim Abou Zeid <karim23697@gmail.com>
2018-08-25 15:14:35 +0200

    Notch support via WindowInsets

--- app/src/main/java/com/kabouzeid/gramophone/ui/fragments/mainactivity/AbsMainActivityFragment.java
+++ app/src/main/java/com/kabouzeid/gramophone/ui/fragments/mainactivity/AbsMainActivityFragment.java
@@ -1,13 +1,8 @@
 package com.kabouzeid.gramophone.ui.fragments.mainactivity;
 
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.view.View;
 
-import com.kabouzeid.appthemehelper.ThemeStore;
-import com.kabouzeid.appthemehelper.util.ColorUtil;
-import com.kabouzeid.gramophone.R;
 import com.kabouzeid.gramophone.ui.activities.MainActivity;
 
 /**
@@ -23,25 +18,5 @@ public abstract class AbsMainActivityFragment extends Fragment {
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setHasOptionsMenu(true);
-    }
-
-    // WORKAROUND
-    public void setStatusbarColor(View view, int color) {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-            final View statusBar = view.findViewById(R.id.status_bar);
-            if (statusBar != null) {
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-                    statusBar.setBackgroundColor(ColorUtil.darkenColor(color));
-                    getMainActivity().setLightStatusbarAuto(color);
-                } else {
-                    statusBar.setBackgroundColor(color);
-                }
-            }
-        }
-    }
-
-    public void setStatusbarColorAuto(View view) {
-        // we don't want to use statusbar color because we are doing the color darkening on our own to support KitKat
-        setStatusbarColor(view, ThemeStore.primaryColor(getContext()));
     }
 }
--- app/src/main/java/com/kabouzeid/gramophone/views/StatusBarMarginFrameLayout.java
+++ app/src/main/java/com/kabouzeid/gramophone/views/StatusBarMarginFrameLayout.java
@@ -3,30 +3,29 @@ package com.kabouzeid.gramophone.views;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
-import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowInsets;
+import android.widget.FrameLayout;
 
-public class StatusBarView extends View {
+public class StatusBarMarginFrameLayout extends FrameLayout {
 
 
-    public StatusBarView(Context context) {
+    public StatusBarMarginFrameLayout(Context context) {
         super(context);
     }
 
-    public StatusBarView(Context context, AttributeSet attrs) {
+    public StatusBarMarginFrameLayout(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public StatusBarView(Context context, AttributeSet attrs, int defStyleAttr) {
+    public StatusBarMarginFrameLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
     }
 
     @Override
     public WindowInsets onApplyWindowInsets(WindowInsets insets) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            ViewGroup.LayoutParams lp = getLayoutParams();
-            lp.height = insets.getSystemWindowInsetTop();
+            MarginLayoutParams lp = (MarginLayoutParams) getLayoutParams();
+            lp.topMargin = insets.getSystemWindowInsetTop();
             setLayoutParams(lp);
         }
         return super.onApplyWindowInsets(insets);
