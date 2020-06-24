b25e75810c5ee6d0bd57d9b5076a3288d89ffee8
Kiarash Golzadeh <kiarash.gl@gmail.com>
2020-06-24 23:09:53 +0430

    Add set music as alarm alert

--- app/src/main/java/com/kabouzeid/gramophone/util/RingtoneManager.java
+++ app/src/main/java/com/kabouzeid/gramophone/util/RingtoneManager.java
@@ -41,7 +41,7 @@ public class RingtoneManager {
                 .show();
     }
 
-    public   void setRingtone(@NonNull final Context context, final int id) {
+    public void setRingtone(@NonNull final Context context, final int id) {
         final ContentResolver resolver = context.getContentResolver();
         final Uri uri = MusicUtil.getSongFileUri(id);
         try {
@@ -64,6 +64,40 @@ public class RingtoneManager {
                     cursor.moveToFirst();
                     Settings.System.putString(resolver, Settings.System.RINGTONE, uri.toString());
                     final String message = context.getString(R.string.x_has_been_set_as_ringtone, cursor.getString(0));
+                    Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
+                }
+            } finally {
+                if (cursor != null) {
+                    cursor.close();
+                }
+            }
+        } catch (SecurityException ignored) {
+        }
+    }
+
+    public void setAlarm(@NonNull final Context context, final int id) {
+        final ContentResolver   resolver = context.getContentResolver();
+        final Uri uri = MusicUtil.getSongFileUri(id);
+        try {
+            final ContentValues values = new ContentValues(2);
+            values.put(MediaStore.Audio.AudioColumns.IS_ALARM, "1");
+            resolver.update(uri, values, null, null);
+        } catch (@NonNull final UnsupportedOperationException ignored) {
+            return;
+        }
+
+        try {
+            Cursor cursor = resolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+                    new String[]{MediaStore.MediaColumns.TITLE},
+                    BaseColumns._ID + "=?",
+                    new String[]{String.valueOf(id)},
+                    null);
+            try {
+                if (cursor != null && cursor.getCount() == 1) {
+                    cursor.moveToFirst();
+                    android.media.RingtoneManager.setActualDefaultRingtoneUri(context, android.media.RingtoneManager.TYPE_ALARM, uri);
+                    Settings.System.putString(resolver, Settings.System.ALARM_ALERT, uri.toString());
+                    final String message = context.getString(R.string.x_has_been_set_as_alarm, cursor.getString(0));
                     Toast.makeText(context, message, Toast.LENGTH_SHORT).show();
                 }
             } finally {
